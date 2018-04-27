c74bb08fedb01cb63fe626634be9246e722a6875
Jake Bowers <jake@jakebowers.org>
2018-04-27 16:18:02 -0500

    file to download data to avoid redownloading all the time

---
+++ Data/downloadData.R
@@ -0,0 +1,3 @@
+## Simply download the data so that we dont have to wait to download everytime we want to change our recoding.
+
+download.file(url("http://jakebowers.org/PS531/anes_timeseries_2016.dta"),destfile = "anes_timeseries_2016.dta")
