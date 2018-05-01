a5a847b9d5bcaa0a4617c9d71cd1337c1ae20b66
Jake Bowers <jake@jakebowers.org>
2018-05-01 15:11:05 -0500

    Adding caption and thus label

--- Figures/thetab.R
+++ Figures/thetab.R
@@ -6,7 +6,7 @@ load(here("Analysis","models.rda"))
 
 summat <- glm1summary[1:7,]
 
-atable <- xtable(summat,label="tab:thetab")
+atable <- xtable(summat,label="tab:thetab", caption="A long description that means that you do not have to read the text of the paper in order to understand the table")
 
 print(atable,file="thetab.tex")
 
